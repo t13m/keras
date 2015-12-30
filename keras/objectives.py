17a8ea74ef2fd14c63d3a25df48894475e76cc5c
zhufengyun <zhufengyun@pku.edu.cn>
2015-12-30 16:26:16 +0800

    test masked obj

--- keras/objectives.py
+++ keras/objectives.py
@@ -37,7 +37,7 @@ def hinge(y_true, y_pred):
 def categorical_crossentropy(y_true, y_pred):
     '''Expects a binary class matrix instead of a vector of scalar classes.
     '''
-    return K.mean(K.categorical_crossentropy(y_pred, y_true), axis=-1)
+    return K.categorical_crossentropy(y_pred, y_true)
 
 
 def binary_crossentropy(y_true, y_pred):
